c7f0fa38c495de9c179f9c763df7561d51a8109f
Javier Iglesias <jaigle@mecon.gob.ar>
2024-04-08 20:40:47 -0300

    version para sgalla

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello Giyo y Memaaa</h1>
+        <h1>Hi Sebas!! :) </h1>
       </header>
     </div>
   );
